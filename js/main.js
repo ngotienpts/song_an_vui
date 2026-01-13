ec3875380721b649966e5048408089548f1638c9
Tien <ngotienpts@gmail.com>
2026-01-13 17:03:43 +0700

    13/1

---
+++ js/main.js
@@ -0,0 +1,302 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Tập hợp tất cả các phần tử cần sử dụng
+    const backTop = document.querySelector("#back-top");
+
+    // Xử lý video tỉ lệ 16:9
+     function handleVideo_16x9() {
+        const video169s = document.querySelectorAll(".js__video169");
+        if (video169s.length === 0) return;
+        video169s.forEach((video169) => {
+            var videos = video169.querySelectorAll("iframe");
+            if (videos.length === 0) return;
+            videos.forEach((video) => {
+                var w = video.offsetWidth;
+                video.style.height = (w * 9) / 16 + "px";
+            });
+        });
+    }
+
+    // xử lý sự kiện để show dropdown
+     function handleShowDropdown() {
+        
+        const dropdownContainers = document.querySelectorAll(".js__dropdownContainer");
+
+
+        if (dropdownContainers.length === 0) return;
+
+
+        dropdownContainers.forEach((dropdownContainer)=>{
+
+            const dropdown = dropdownContainer.querySelector(".js__showDropdown");
+            const dropdownContent = dropdownContainer.querySelector(".js__dropdownContent");
+            const overlay = dropdownContainer.querySelector(".js__overlay");
+
+
+            dropdown.onclick = function () {
+                dropdownContent.classList.toggle("active");
+                overlay.classList.add('active')
+
+            };
+
+            overlay.onclick = function () {
+                dropdownContent.classList.remove("active");
+                this.classList.remove("active");
+            };
+        })
+
+      
+    }
+
+    // Khởi tạo slider với một item
+    function initSliderOneItems() {
+        const oneSlides = document.querySelectorAll(".js__oneSlidesContainer");
+        if (oneSlides) {
+            oneSlides.forEach((item) => {
+                var slider = item.querySelector(".js__oneSlide");
+                var next = item.querySelector(".swiper-button-next");
+                var prev = item.querySelector(".swiper-button-prev");
+                var pagi = item.querySelector(".swiper-pagination");
+
+                new Swiper(slider, {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                    slidesPerGroup: 1,
+                    navigation: {
+                        nextEl: next || null,
+                        prevEl: prev || null,
+                    },
+                    pagination: {
+                        el: pagi,
+                        clickable: true,
+                    },
+                    // autoplay: {
+                    //     delay: 3000,
+                    //     disableOnInteraction: false,
+                    // },
+                });
+            });
+        }
+    }
+
+    // khởi tạo slider với 3 item
+    function initSliderThreeItems() {
+        const threeSlides = document.querySelectorAll(".js__threeSlidesContainer");
+        if (threeSlides) {
+            threeSlides.forEach((item) => {
+                var slider = item.querySelector(".js__threeSlides");
+                var next = item.querySelector(".swiper-button-next");
+                var prev = item.querySelector(".swiper-button-prev");
+                var pagi = item.querySelector(".swiper-pagination");
+                new Swiper(slider, {
+                    slidesPerView: 1,
+                    spaceBetween: 15,
+                    slidesPerGroup: 1,
+                    navigation: {
+                        nextEl: next || null,
+                        prevEl: prev || null,
+                    },
+                    pagination: {
+                        el: pagi || null,
+                        clickable: true,
+                    },
+                    // autoplay: {
+                    //     delay: 3000,
+                    //     disableOnInteraction: false,
+                    // },
+                    breakpoints: {
+                        768: {
+                            slidesPerView: 2,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 20,
+                        }
+                    },
+                });
+            });
+        }
+    }
+
+    
+    // xử lý sự kiện show menu mobile
+    function handleMenuMobile () {
+        // show menu
+        const bod = document.querySelector('body')
+        const clickShowMenuMbs = document.querySelectorAll('.js__clickShowMenuMb');
+        const closeSubMenuMb = document.querySelector('.js__closeSubMenuMb');
+        const subMenuMb = document.querySelector('.js__subMenuMb');
+
+        if(clickShowMenuMbs.length === 0 && !closeSubMenuMb) return;
+        clickShowMenuMbs.forEach((clickShowMenuMb)=>{
+            clickShowMenuMb.onclick = function() {
+                subMenuMb.classList.add('active');
+                bod.classList.add('hidden')
+            }
+        })
+        closeSubMenuMb.onclick = function(){
+            subMenuMb.classList.remove('active');
+            bod.classList.remove('hidden')
+        }
+
+        const subMenuMbContainers = document.querySelectorAll('.js__subMenuMbContainer');
+
+        if (subMenuMbContainers.length === 0 ) return;
+        
+        subMenuMbContainers.forEach((subMenuMbContainer)=>{
+
+            const subMenuMbItems = subMenuMbContainer.querySelectorAll('.js__subMenuMbItem');
+            const subMenuMbDropdowns = subMenuMbContainer.querySelectorAll('.js__subMenuMbDropdown');
+            
+            if (subMenuMbItems.length === 0 ) return;
+            
+            subMenuMbItems.forEach((subMenuMbItem)=>{
+                const showSubMenuMbItem = subMenuMbItem.querySelector('.js__showSubMenuMbItem');
+                
+                if(!showSubMenuMbItem) return
+
+                showSubMenuMbItem.onclick = function() {
+                    subMenuMbItem.classList.toggle('active')
+                }
+            });
+
+            if (subMenuMbDropdowns.length === 0 ) return;
+
+            subMenuMbDropdowns.forEach((subMenuMbDropdown)=>{
+                const showSubMenuMbDropdown = subMenuMbDropdown.querySelector('.js__showSubMenuMbDropdown');
+                
+                if(!showSubMenuMbDropdown) return
+
+                showSubMenuMbDropdown.onclick = function() {
+                    subMenuMbDropdown.classList.toggle('active')
+                }
+            });
+
+            
+
+        });
+
+    }
+
+    // xử lý sự kiện show search pc
+    function handleShowSearchPc () {
+        const searchPc = document.querySelectorAll(".js__searchPc");
+        if (searchPc.length === 0) return;
+        searchPc.forEach((searchPc) => {
+           
+            var iconSearchPc = document.querySelector(".js__searchPcIcon");
+            var formSearchPc = document.querySelector(".js__formSearchPc");
+            const focusElement =
+                formSearchPc.querySelector(".js__focusSearchPc");
+            iconSearchPc.onclick = function () {
+                searchPc.classList.toggle('active')
+                formSearchPc.classList.add("active");
+                focusElement.focus();
+
+                if(!searchPc.classList.contains('active')) {
+                    formSearchPc.classList.remove("active");
+                    focusElement.value = "";
+                }
+                
+            };
+        });
+        
+    }
+    // xử lý sự kiện show search mb
+    function handleShowSearchMb () {
+        const searchMbs = document.querySelectorAll(".js__searchMb");
+        if (searchMbs.length === 0) return;
+        searchMbs.forEach((searchMb) => {
+            var closeSearchMb =
+                document.querySelector(".js__closeSearchMb");
+            var formSearchMb = document.querySelector(".js__formSearchMb");
+            const focusElement =
+                formSearchMb.querySelector(".js__focusSearchMb");
+            searchMb.onclick = function () {
+                formSearchMb.classList.add("active");
+                focusElement.focus();
+                
+            };
+            closeSearchMb.onclick = function () {
+                formSearchMb.classList.remove("active");
+                focusElement.value = "";
+            };
+        });
+        
+    }
+
+
+
+    // Khởi tạo fancybox
+    function initFancybox() {
+        const fancyboxes = document.querySelectorAll(".fancybox-full");
+        if (fancyboxes) {
+            fancyboxes.forEach(function () {
+                $(".fancybox-full a").fancybox();
+            });
+        }
+    }
+
+    // Xử lý thanh header dính
+    function handleStickyHeader() {
+        const stickyHeaderPC = document.querySelector(".js__stickyHeader");
+        if (stickyHeaderPC) {
+            const isSticky = scrollY > 300;
+            stickyHeaderPC.classList.toggle("sticky", isSticky);
+        }
+    }
+
+    // Xử lý sự kiện khi nhấn nút "back to top"
+    function handleBackTop() {
+    
+        if (!backTop) return;
+
+        backTop.onclick = function () {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        };
+
+    }
+
+    // Xử lý hiển thị nút backTop dựa trên vị trí cuộn trang
+    function handleBackTopVisibility() {
+        if (backTop) {
+            if (
+                document.body.scrollTop > 300 ||
+                document.documentElement.scrollTop > 300
+            ) {
+                backTop.style.opacity = 1;
+                backTop.style.visibility = "visible";
+            } else {
+                backTop.style.opacity = 0;
+                backTop.style.visibility = "hidden";
+            }
+        }
+    }
+
+    // Xử lý sự kiện khi cuộn trang
+    function handleWindowScroll() {
+        handleStickyHeader();
+        handleBackTopVisibility()
+    }
+
+    // Khởi tạo tất cả các chức năng
+    function initApp() {
+        handleShowSearchPc();
+        handleShowSearchMb();
+        handleMenuMobile();
+        handleShowDropdown();
+        // slide
+        initSliderOneItems();
+        initSliderThreeItems();
+        
+        // end slide
+        handleBackTop();
+        initFancybox();
+        handleVideo_16x9();
+        window.addEventListener('scroll',handleWindowScroll);
+        window.addEventListener('resize',handleWindowScroll);
+    }
+
+    // Bắt đầu khởi tạo ứng dụng
+    initApp();
+});
